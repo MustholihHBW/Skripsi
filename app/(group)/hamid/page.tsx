0eefcf454ee82e06fb5126fbe73f9f03d2f45c90
Hamid <alhamid.mustholih97@gamil.com>
2026-02-13 05:43:34 +0700

    menambahkan fitur next.route

---
+++ app/(group)/hamid/page.tsx
@@ -0,0 +1,9 @@
+const HamidPage = () => {
+  return (
+    <div>
+      <h1>This is Hamid Page</h1>
+    </div>
+  );
+};
+
+export default HamidPage;
---
+++ app/(group)/routea/page.tsx
@@ -0,0 +1,9 @@
+const RouteA = () => {
+  return (
+    <div>
+      <h1>This is Route A</h1>
+    </div>
+  );
+};
+
+export default RouteA;
