b73dcc65274580026253a155bb1c9021a357641f
Hamid <alhamid.mustholih97@gamil.com>
2026-02-15 21:46:47 +0700

    menambahkan createRouteMatcher untuk memberikan proteksi ke semua routes

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,3 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return <p>This is a protected Dashboard Page</p>;
+  return (
+    <div>
+      <UserButton afterSwitchSessionUrl="/" />
+    </div>
+  );
 }
