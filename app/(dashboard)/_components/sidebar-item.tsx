2cc33eee51d64f4bb43c297c61f22703476224a0
Hamid <alhamid.mustholih97@gamil.com>
2026-02-21 23:57:52 +0700

    menambahkan function pathname dan isActive untuk sidebarItem

--- app/(dashboard)/_components/sidebar-item.tsx
+++ app/(dashboard)/_components/sidebar-item.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { LucideIcon } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
 
 interface SidebarItemProps {
     icon: LucideIcon
@@ -13,6 +14,14 @@ export const SidebarItem = ({
     label,
     href,
 }: SidebarItemProps) => {
+    const pathname = usePathname();
+    const router = useRouter(); 
+
+    const isActive =
+        (pathname === "/" && href === "/") ||
+        pathname === href ||
+        pathname?.startsWith(`${href}/`);
+
     return (
         <div>
             SidebarItem
