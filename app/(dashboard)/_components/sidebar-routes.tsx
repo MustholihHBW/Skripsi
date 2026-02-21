53371016ea904cd7cd324406c03b31386b3d3d0d
Hamid <alhamid.mustholih97@gamil.com>
2026-02-21 21:47:20 +0700

    menambahkan sidebar-routes untuk sidebar

--- app/(dashboard)/_components/sidebar-routes.tsx
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -1,9 +1,36 @@
 "use client";
 
+import { Compass, Icon, icons, Layout, Sidebar } from "lucide-react";
+import { SidebarItem } from "./sidebar-item";
+
+const guestRoutes = [
+    {
+        Icon: Layout,
+        label: "Dashboard",
+        href: "/",
+    },
+    {
+        Icon: Compass,
+        label: "Browse",
+        href: "/search",
+    },
+];
+
+
+
 export const SidebarRoutes = () => {
+    const routes = guestRoutes;
+
     return (
         <div className="flex flex-col w-full">
-            Routes
+            {routes.map((route) => (
+                <SidebarItem
+                    key={route.href}
+                    icon={route.Icon}
+                    label={route.label}
+                    href={route.href}
+                />
+            ))}
         </div>
     );
 }
