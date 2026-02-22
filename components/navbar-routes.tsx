8d34fdc5fc4af1cce2940ec0e653d970bb93268a
Hamid <alhamid.mustholih97@gamil.com>
2026-02-22 23:04:36 +0700

    Menambahkan UserButton untuk navbar

---
+++ components/navbar-routes.tsx
@@ -0,0 +1,11 @@
+"use cliet"
+
+import { UserButton } from "@clerk/nextjs"
+
+export const NavbarRoutes =() => {
+    return (
+        <div className="flex gap-x-2 ml-auto">
+            <UserButton/>
+        </div>
+    )
+}
