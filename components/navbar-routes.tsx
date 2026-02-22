40eae46c46169b42245dc9425c22c5a257937c51
Hamid <alhamid.mustholih97@gamil.com>
2026-02-23 05:43:38 +0700

    menambahkan halaman course dan exit button untuk teacher mode

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -17,7 +17,7 @@ export const NavbarRoutes =() => {
     return (
         <div className="flex gap-x-2 ml-auto">
             {isTeacherPage || isPlayerPage ? (
-                <Button>
+                <Button size="sm" variant="ghost">
                     <LogOut className="h-4 w-4 mr-2"/>
                     Exit
                 </Button>
