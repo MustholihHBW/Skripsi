5450983a83cc2ee99e5e3a610e54170f92b36734
Hamid <alhamid.mustholih97@gamil.com>
2026-02-23 22:47:55 +0700

    menambahkan styling untuk course tittle

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -11,6 +11,7 @@ import {
     FormControl,
     FormDescription,
     FormField,
+    FormItem,
     FormLabel,
     FormMessage,
 
@@ -40,8 +41,41 @@ const CreatePage = () => {
     }
 
     return (  
-        <div>
-            Create page
+        <div className="max-w-5xl mx-auto flex md:items-center md:justify-center h-full p-6">
+            <div>
+                <h1 className="text-2xl">
+                    Name your Course
+                </h1>
+                <p className="text-sm text-slate-600">
+                    what would you like to name your course? Don&apos;t worry, you can always change it later.
+                </p>
+                <Form {...form}>
+                    <form 
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className="space-y-8 mt-8"
+                    >
+                        <FormField
+                            control={form.control}
+                            name="title"
+                            render={({field}) => (
+                                <FormItem>
+                                    <FormLabel>
+                                        Course Title
+                                    </FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            disabled={isSubmitting}
+                                            placeholder="e.g 'Fiqih safinatun Najah bab Wudlu'"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                </FormItem>
+                            )}
+                        />
+                    </form>
+
+                </Form>
+            </div>
         </div>
     );
 }
